13c45e49b3eb673b273dbe56caa4d930f67070c1
Ümit Yıldırım <umit.yildirim53@hotmail.com>
2022-12-08 19:57:21 +0300

    added socket.io library

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+const io = socketio(server);
+
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("new websocket connection");
+
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+    //socket.emit("countUpdated", count);
+    io.emit("countUpdated", count);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
