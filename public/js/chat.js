3fa05f467b1139e6e12764c45f66201d3c06f46d
Ümit Yıldırım <umit.yildirim53@hotmail.com>
2022-12-10 00:37:08 +0300

    added timestamp and css files

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,23 +1,70 @@
 const socket = io();
 
+//Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocation = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+
 socket.on("message", (message) => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format("HH:mm"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+socket.on("locationMessage", (location) => {
+  console.log(location);
+  const html = Mustache.render(locationMessageTemplate, {
+    url: location.url,
+    createdAt: moment(location.createdAt).format("HH:mm"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered");
+  });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocation.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+  $sendLocation.setAttribute("disabled", "disabled");
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      lat: position.coords.latitude,
-      lon: position.coords.longitude,
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      },
+      () => {
+        console.log("Location Shared");
+        $sendLocation.removeAttribute("disabled");
+      }
+    );
   });
 });
